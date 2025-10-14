52643287eb0a7274c351f5bdd405d795ee0fa39b
xut-e <xut.workmail@gmail.com>
2025-10-14 17:09:05 +0200

    Funcionalidad extra

--- Procesos/Practica/ej2/ej2_v1.cpp
+++ Procesos/Practica/ej2/ej2_v1.cpp
@@ -10,15 +10,23 @@
 
 #define HIJOS 3
 #define EXTREMOS 2
+#define CAMPOS 2
 #define LOG "registro.txt"
 #define LOGFILTRADO "coincidencias.txt"
 
+struct campos{
+    char caracteres[256];
+    int lineas;
+};
+
 int main(){
 
-    int AB[EXTREMOS], BC[EXTREMOS];
+    int AB[EXTREMOS], BC[EXTREMOS], DC[EXTREMOS];
+    campos func_extra;
 
     pipe(AB);
     pipe(BC);
+    pipe(DC);
 
     for (int i = 0; i < HIJOS; i++)
     {
@@ -29,6 +37,8 @@ int main(){
                 std::string texto_escrito;
 
                 close(AB[0]);
+
+                std::cout << "Introduce el texto a capturar (escribe END para finalizar):" << std::endl;
 
                 while (std::getline(std::cin, texto_escrito))
                 {
@@ -48,6 +58,8 @@ int main(){
                 std::vector<char> texto_leido(1024);
                 ssize_t bytes_leidos; //signed size type, si devuelve un numero positivo es que hay bytes, si devuelve 0 es EOF y si devuelve -1 es error, por eso debe ser ssize_t y no size_t
                 
+                int lineas = 0;
+
                 close(AB[1]);
                 close(BC[0]);
 
@@ -65,11 +77,46 @@ int main(){
                     fflush(archivo); //usamos fflush para forzar la escritura en vez de que se quede guardado en RAM
 
                     write(BC[1], texto_leido.data(), bytes_leidos); //Era mucho más fácil controlar cuándo debía C empezar a leer mediante pipes que mediante archivos.
+                    lineas++;
                 }
                 
                 close(AB[0]);
                 close(BC[1]);
                 fclose(archivo);
+
+                campos *punteroAFuncExtra = &func_extra;
+
+                if (fork() == 0)
+                {
+                    std::cout << "Introduce los caracter/es por el/los que quieras filtrar: ";
+                    std::cin >> func_extra.caracteres;
+
+                    std::cout << "Introduce la cantidad de lineas que quieras mostrar: ";
+
+                    do
+                    {
+                        std::cin >> func_extra.lineas;
+
+                        if(func_extra.lineas < 1 || func_extra.lineas > 20){
+                            std::cout << "Introduce un numero entre 1 y 20!" << std::endl;
+                        }
+                        if(func_extra.lineas > lineas){
+                            std::cout << "Introduce un número menor o igual que " << lineas << "!" << std::endl;
+                        }
+                        
+                    } while (func_extra.lineas < 1 || func_extra.lineas > 20 || func_extra.lineas > lineas);
+
+                    close(DC[0]);
+
+                    write(DC[1], punteroAFuncExtra, sizeof(campos));
+
+                    close(DC[1]);
+                    
+                    exit(0);
+                }
+                
+                wait(NULL);
+
                 exit(0);
             }
             else if (i == 2)
@@ -77,6 +124,18 @@ int main(){
                 close(AB[0]);
                 close(AB[1]);
                 close(BC[1]);
+
+                campos inputsLeidos;
+                campos *punteroAInputsLeidos = &inputsLeidos;
+
+                close(DC[1]);
+
+                read(DC[0], punteroAInputsLeidos, sizeof(campos));
+
+                close(DC[0]);
+
+                std::string caracteres = inputsLeidos.caracteres;
+                int lineas;
 
                 FILE *escritura = fopen(LOGFILTRADO, "w");
 
@@ -96,12 +155,21 @@ int main(){
                 
 
                 std::vector<char> archivo_leido(1024);
+                int lineas_restantes = 0;
 
-                while (fgets(archivo_leido.data(), sizeof(archivo_leido), lectura))
+                while (fgets(archivo_leido.data(), archivo_leido.size(), lectura))
                 {
-                    if (strchr(archivo_leido.data(), '@'))
+                    if (lineas_restantes > 0)
                     {
                         fputs(archivo_leido.data(), escritura);
+                        lineas_restantes--;
+                        continue;
+                    }
+                    
+                    if (strstr(archivo_leido.data(), caracteres.c_str()))
+                    {
+                        fputs(archivo_leido.data(), escritura);
+                        lineas_restantes = inputsLeidos.lineas - 1;
                     }
                 }
 
@@ -109,7 +177,7 @@ int main(){
                 fclose(escritura);
                 
                 exit(0);
-            }
+            }            
         }
     }
 
@@ -117,6 +185,8 @@ int main(){
     close(AB[1]);
     close(BC[0]);
     close(BC[1]);
+    close(DC[0]);
+    close(DC[1]);
 
 
     for (int i = 0; i < HIJOS; i++)
